a6b9a089fcc0a3d49097cbab9aa454d558fa526b
Ahmad-Diish <albasha.9669@gmail.com>
2024-05-04 15:05:15 +0200

    Ahmad

--- Pages/genres.php
+++ Pages/genres.php
@@ -9,49 +9,25 @@ $genrei = new GenreRepository($conn);
 
 ?>
 <genres>
+
     <head>
         <style>
             body {
                 background-color: #fffbeb;
-             
-            }
 
-            
+            }
 
             .title {
                 text-align: center;
-                font-size: 3rem;
-                margin-bottom: 2rem;
-                text-transform: uppercase;
-                letter-spacing: 2px;
-                position: relative;
-                overflow: hidden;
                 color: #923f0e;
-            }
+                font-family: "Goudy Stout";
+                margin-TOP: 70px;
+                margin-bottom: 100px;
 
-            .title::before {
-                content: 'Genres Galerie';
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                color: #fef3c7;
-                overflow: hidden;
-                animation: animate 3s linear infinite;
             }
-
-            @keyframes animate {
-                0% {
-                    transform: translateY(-100%);
-                }
-                100% {
-                    transform: translateY(100%);
-                }
-            }
-
         </style>
     </head>
+
     <body>
         <div class="container mt-4">
             <h2 class="title">Genres Galerie</h2>
@@ -67,8 +43,4 @@ $genrei = new GenreRepository($conn);
 
 
 <?php require_once("../Homepage/footer.php");
- ?>
-
-
-
-
+?>
