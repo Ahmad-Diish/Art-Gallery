25fe1b02247240962d4a59c9411da5c9a6602d0b
Keller, Saskia <saskia.keller@th-wildau.de>
2024-05-25 13:53:20 +0200

    Suchseite angepasst

---
+++ Search/search.php
@@ -0,0 +1,24 @@
+<?php
+require_once("../Homepage/header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <h3>Suchergebnisse</h3>
+    
+</body>
+</html>
+
+
+<?php
+require_once("../Homepage/footer.php");
+?>
