0fbb2375b5c29f313f0e25803bb70cfd1cb4dc17
RB963 <ramibalasem123@gmail.com>
2024-06-03 18:28:49 +0200

    Rami

--- Homepage/footer.php
+++ Homepage/footer.php
@@ -17,6 +17,18 @@
       color: white;
       text-decoration: none;
     }
+        .social-icons {
+      margin-top: 10px;
+   
+    }
+
+    .social-icons a {
+      margin-right: 10px;
+      color: #ffffff;
+      font-size: 1.5em;
+      
+    }
+    
   </style>
 </head>
 
@@ -28,10 +40,22 @@
         <div class="col-md-4">
           <h3>Kontakt</h3>
           <ul class="list-unstyled">
-            <li><i class="fas fa-envelope"></i> Adresse: <br> <a style="color: #ffffff;"> Art Gallery <br> Hochschulring 1 <br> 15745 Berlin</a></li>
+          <li>
+            <i class="fas fa-map-marker-alt"></i> Art Gallery <br>
+            <a href="https://www.google.de/maps/place/Technische+Hochschule+Wildau/@52.3186348,13.6283525,17z/data=!3m1!4b1!4m6!3m5!1s0x47a83ee868af915b:0x93a6fcb46dcb4367!8m2!3d52.3186315!4d13.6309274!16zL20vMGY2Zm5z?entry=ttu"style="color: #ffffff;"
+               class="address-link"> Hochschulring 1 <br> 15745 Wildau</a>
+        </li>
             <li><i class="fas fa-envelope"></i> Email: <a href="mailto:info@beispiel.com" style="color: #ffffff;">info@Art-Gallery.com</a></li>
             <li><i class="fas fa-phone"></i> Telefon: <a href="tel:+49123456789" style="color: #ffffff;">+49 123 456789</a></li>
           </ul>
+          <div class="social-icons">
+          <a href="https://github.com/Ahmad-Diish/Art-Gallery" > <i class="fab fa-github"></i></a>
+            <a href="https://www.instagram.com/" ><i class="fab fa-instagram"></i></a>
+            <a href="https://facebook.com/" ><i class="fab fa-facebook"></i></a>
+            <a href="https://www.craiyon.com/image/Gz9QrKKZRW603sUmRqNSPw" ><i class="fas fa-globe"></i></a>
+            <a href="../Pages/aboutus.php" ><i class="fas fa-info-circle"></i></a>
+          </div>
+        
         </div>
         <div class="col-md-4">
           <h3>Über Uns</h3>
